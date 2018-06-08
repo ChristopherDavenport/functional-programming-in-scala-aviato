f17c4e9f01ee705f8f81e089d65d4cb5eb2d819d
Chris Davenport <chris@christopherdavenport.tech>
2018-06-08 12:31:32 -0400

    Add MOption Functions

--- src/main/scala/fpis/chapter04/Chapter4.scala
+++ src/main/scala/fpis/chapter04/Chapter4.scala
@@ -28,6 +28,28 @@ object Chapter4 {
       case MSome(a) => ifSome(a)
       case MNone => ifNone
     }
+    def map[A, B](ma: MOption[A])(f: A => B): MOption[B] = fold(ma)(MOption.none[B])(a => MOption.some(f(a)))
+    def flatMap[A, B](ma: MOption[A])(f: A => MOption[B]): MOption[B] = fold(ma)(MOption.none[B])(f)
+    def getOrElse[A](ma: MOption[A])(default: A): A = fold(ma)(default)(identity)
+    def orElse[A](ma: MOption[A])(mb: => MOption[A]): MOption[A] = fold(ma)(mb)(MOption.some)
+    def select[A](ma: MOption[A])(f: A => Boolean): MOption[A] = fold(ma)(MOption.none[A])(a => if (f(a)) MOption.some(a) else MOption.none)
+    def filter[A](ma: MOption[A])(f: A => Boolean): MOption[A] = select(ma)(f)
+
+    def lift[A,B](f: A => B): MOption[A] => MOption[B] = map(_)(f)
+    def catchThrowable[A](a: => A): MOption[A] = 
+      try { MOption.some(a) } catch { case _: Exception => MOption.none[A] }
+
+    def map2[A,B, C](ma: MOption[A], mb: MOption[B])(f: (A, B) => C): MOption[C] = 
+      flatMap(ma)(a => map(mb)(b => f(a,b)))
+
+    def sequence[A](xa: List[MOption[A]]): MOption[List[A]] = 
+      xa.foldRight(MOption.some(List.empty[A])){case (next, acc) => flatMap(acc)(l => map(next)(a => a::l))}
+
+    def traverse[A, B](xa: List[A])(f: A => MOption[B]): MOption[List[B]] = 
+      xa.foldRight(MOption.some(List.empty[B])){case (next, acc) => flatMap(acc)(l => map(f(next))(a => a::l))}
+
+    def sequenceViaTraverse[A](xa: List[MOption[A]]): MOption[List[A]] = traverse(xa)(identity)
+
     private final case class MSome[A](value: A) extends MOption[A]
     private final case object MNone extends MOption[Nothing]
   }
